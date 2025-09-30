1b2d1f0cdc49ad78cca7c458c86cd9a9b967d98f
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2025-09-29 20:14:20 -0400

    added a save to local storage function and add a task function

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -1,15 +1,31 @@
-todoList = [{
-    name: 'task 123',
-    date: '12/56/12',
-    id: ''
-}, {
-    name: 'task 1234',
-    date: '12/56/12',
-    id: ''
-}, 
-]
+//Load and save data
+let todoList;
+loadList();
+function loadList() {
+    todoList = JSON.parse(localStorage.getItem('myToDoList'))
+
+if (!todoList) {
+    todoList = [{
+        name: 'task 123',
+        date: '12/56/12',
+        id: ''
+    }, {
+        name: 'task 1234',
+        date: '12/56/12',
+        id: ''
+    }, 
+    ]
+}
+}
+
+function saveList() {
+    localStorage.setItem('myToDoList', JSON.stringify(todoList))
+}
 
 
+
+
+//Generating HTML
 function todoListHTML() {
     let HTML = '';
     todoList.forEach((item) => {
@@ -20,10 +36,25 @@ function todoListHTML() {
         <div class = "tableElement">Edit #4</div>
     </div>`
     }) 
-
     document.querySelector('.toDoList').innerHTML = HTML;
-
 }
 
+//Functionality
+
+    //Addto Cart
+    document.querySelector('.js-taskAddButton').addEventListener("click", () => {
+        const taskName = document.querySelector('.js-name-input').value;
+        const taskDate = document.querySelector('.js-date-input').value;
+        todoList.push({
+            name: taskName,
+            date: taskDate,
+        });
+        todoListHTML()
+        saveList()
+    })
+
+
+
+
+
 todoListHTML();
-console.log(todoList)
